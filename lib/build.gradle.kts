8d74812b9b44d87eb2aff47c8826df24adb153ad
DotsTeaLab <dotstealab@gmail.com>
2023-04-11 07:28:36 +0200

    add files

---
+++ lib/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+	alias(libs.plugins.com.android.library)
+	alias(libs.plugins.org.jetbrains.kotlin.android)
+	`maven-publish`
+}
+
+android {
+	namespace = "com.number869.seksinavigation"
+	compileSdkPreview = "UpsideDownCake"
+
+	defaultConfig {
+		minSdk = 33
+		targetSdk = 33
+
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		vectorDrawables {
+			useSupportLibrary = true
+		}
+	}
+
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
+	buildFeatures {
+		compose = true
+	}
+	composeOptions {
+		kotlinCompilerExtensionVersion = "1.4.4"
+	}
+	packaging {
+		resources {
+			excludes += "/META-INF/{AL2.0,LGPL2.1}"
+		}
+	}
+	publishing {
+		singleVariant("release") {
+			withSourcesJar()
+			withJavadocJar()
+		}
+	}
+}
+
+dependencies {
+	implementation(libs.activity.compose)
+	implementation(platform(libs.compose.bom))
+	implementation(libs.ui)
+	implementation(libs.ui.graphics)
+	implementation(libs.material3)
+	testImplementation(libs.junit)
+	androidTestImplementation(libs.androidx.test.ext.junit)
+	androidTestImplementation(libs.espresso.core)
+	androidTestImplementation(platform(libs.compose.bom))
+	androidTestImplementation(libs.ui.test.junit4)
+	debugImplementation(libs.ui.tooling)
+	debugImplementation(libs.ui.test.manifest)
+}
+
+publishing {
+	publications {
+		register<MavenPublication>("release") {
+			groupId = "com.number869"
+			artifactId = "seksinavigation"
+			version = "0.0.1"
+
+			afterEvaluate {
+				from(components["release"])
+			}
+		}
+	}
+}
