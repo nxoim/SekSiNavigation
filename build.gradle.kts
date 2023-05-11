b85bccb68ef95c60eb6d02d2b25903362bf8a3df
DotsTeaLab <dotstealab@gmail.com>
2023-05-11 22:45:20 +0200

    - updated gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 buildscript {
-	val agp_version by extra("8.2.0-alpha01")
+	val agp_version by extra("8.2.0-alpha03")
 	dependencies {
 		classpath("com.android.tools.build:gradle:$agp_version")
 	}
